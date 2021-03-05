0f305935b02e5b9049d025436d2783a92784ed85
ChesterEucrine <blessingdlamini24747@gmail.com>
2021-03-06 01:01:39 +0200

    fixed failed assertion 2

--- Tests/studentTest.php
+++ Tests/studentTest.php
@@ -5,7 +5,7 @@ require_once "DatabaseHelper.php";
 	class studentTest extends PHPUnit\Framework\TestCase {
 		public function testConstructor() {
 			$expectedNetid = "12";
-			$expectedFullName = "Test";
+			$expectedFirstName = "Test";
 			$expectedLastName = "Name";
 			$expectedFlowChart = json_encode(array("text"=>"example", "array"=>array("sub"=>"example2")));
 
@@ -13,7 +13,7 @@ require_once "DatabaseHelper.php";
 			$student = new Student("12", "Test", "Name", $flowchart);
 			// Assert Values
 			$this->assertEquals($expectedNetid, $student->netid);
-			$this->assertEquals($expectedFullName, $student->full_name);
+			$this->assertEquals($expectedFirstName, $student->first_name);
 			$this->assertEquals($expectedLastName, $student->last_name);
 			$this->assertEquals($expectedFlowChart, $student->flowchart);
 		}
