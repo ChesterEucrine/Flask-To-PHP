c5cb857b70e840276e4ad010d18b98f6393ec558
ChesterEucrine <blessingdlamini24747@gmail.com>
2021-03-08 03:08:41 +0200

    Changed Project structure and added password functions from app.py and addClassReq.py

---
+++ includes/appFunctions.php
@@ -0,0 +1,24 @@
+<?php
+
+class App {
+	private $PASSWORD_FILE = "PASSWORD_HASH_DO_NOT_COMMIT";
+
+	public function change_password($newPassword) {
+		$file = fopen($PASSWORD_FILE, "w");
+		$update = hash("sha256", $newPassword);
+		fwrite($file, $update);
+		fclose($file);
+		return 0;
+	}
+
+	public function is_correct_password($password) {
+		if (!file_exists($PASSWORD_FILE)) {
+			echo "Advisor password not set.";
+			return false;
+		} else {
+			$file = fopen($PASSWORD_FILE, "r");
+			$correctPasswordHash = fread($file);
+			return password_verify($password, $correctPasswordHash);
+		}
+	}
+}
