07f6c4b1623a579a65fbf09e20454790964a3006
ChesterEucrine <blessingdlamini24747@gmail.com>
2021-03-08 02:12:42 +0200

    Course Requirements tests

---
+++ Tests/course_reqTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "DatabaseHelper.php";
+
+class Course_ReqTest extends PHPUnit\Framework\TestCase {
+	public function testConstructor() {
+		$expCourse_no = "CS3340";
+		$expCourse_prereq = "2 CS2305 CS1337";
+		$expCourse_coreq = "0";
+
+		$course = new Course_Req("CS3340", "2 CS2305 CS1337", "0");
+
+		// Asser Values
+		$this->assertEquals($expCourse_no, $couse->course_no);
+		$this->assertEquals($expCourse_prereq, $course->course_prereq);
+		$this->assertEquals($expCourse_coreq, $course->course_coreq);
+	}
+}
