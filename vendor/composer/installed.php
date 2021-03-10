43968b1c29d8ae88897794bbfe16cedac5eee418
ChesterEucrine <blessingdlamini24747@gmail.com>
2021-03-10 23:57:52 +0000

    Composer Setup for php dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '649e596163fb38c5ff6a17ae6e506f89b2c0fceb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '649e596163fb38c5ff6a17ae6e506f89b2c0fceb',
+    ),
+    'spatie/pdf-to-image' => 
+    array (
+      'pretty_version' => '2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ea81a2d5f4fb0ca9f4c4fc0d4f8813135e993b2d',
+    ),
+  ),
+);
